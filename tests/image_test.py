aa514182cf35f4497d658f78e7df3d043278970e
Tomek Mrugalski <tomasz.mrugalski@eti.pg.edu.pl>
2020-05-01 14:40:46 +0200

    Test image is being downloaded automatically

--- tests/image_test.py
+++ tests/image_test.py
@@ -1,8 +1,33 @@
 from noaatools.imageproc import process_img
 import unittest
 import pytest
+import os
+import subprocess
 
 class Georefests(unittest.TestCase):
+    def setUp(self):
+        self.get_test_data()
+
+    TEST_URL = "https://satnogs.klub.com.pl/data/ab802ca1-419f-418a-aeea-d99bb9c702aa-0-1c60fdf5-3f18-409d-9834-17f014c608c1_product.png"
+    TEST_FILE = "tests/1276.png"
+
+    def get_test_data(self):
+        """This ensures the data file is here. We don't want to commit 3MB data to this repo."""
+        try:
+            x = os.stat(self.TEST_FILE)
+            print("File %s exists, skipping download." % self.TEST_FILE)
+        except FileNotFoundError:
+            print("Test data file %s missing, downloading..." % self.TEST_FILE)
+            args = [ "wget -nd %s -O %s" % (self.TEST_URL, self.TEST_FILE) ]
+            process = subprocess.run(args, capture_output=True, shell=True)
+            if process.returncode != 0:
+                print("wget failed to download file. details:\n===STDOUT===")
+                print(process.stdout)
+                print("===STDERR===")
+                print(process.stderr)
+            self.assertEqual(process.returncode, 0)
+            print("Output file stored in %s" % self.TEST_FILE)
+
     def test_ellipsoids(self):
 
         params = {
