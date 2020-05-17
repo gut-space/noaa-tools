b370d587ec97a31e23edac42d898c7ff57d70ae3
Tomek Mrugalski <tomasz.mrugalski@eti.pg.edu.pl>
2020-05-17 13:50:08 +0200

    Constants moved to separate file

---
+++ noaatools/constants.py
@@ -0,0 +1,29 @@
+from enum import Enum
+from collections import namedtuple
+
+# Conversion between radians and degrees
+DEG2RAD = 0.017453292519943296
+RAD2DEG = 57.295779513082321
+
+RE = 6378.137 # Earth radius (in km)
+
+class Method(Enum):
+    SPHERICAL = 1
+    OBLATE = 2
+    PYMAP3D = 3
+
+# This defines ellipsoid (a = equatorial radius in km, finv = inverse flattening)
+Ellipsoid = namedtuple('Ellipsoid', "a finv")
+
+# Source: https://en.wikipedia.org/wiki/Earth_ellipsoid#Historical_Earth_ellipsoids
+ellipsoid_wgs84 = Ellipsoid(a = RE, finv = 298.257223563)
+
+# Sat image aquisition and image transmission is not instanteneous. There is some delay. It's small, but
+# bacause the sat moves at speeds of more than 7km/s, even fractions of second make a big deal of a difference.
+# Not sure if this data is available anywhere. I think it'll have to be picked with trial-and-error.
+# This is expressed in seconds.
+NOAA_PROCESSING_DELAY = 0.0
+
+# This is the FOV (field of view) angle for the observation instrument on NOAA sats for each side.
+# In simple words this is how far the camera can look sideways. The total viewing angle is this multipled by 2.
+AVHRR_ANGLE = 54.3
--- noaatools/georef.py
+++ noaatools/georef.py
@@ -35,33 +35,12 @@ from pymap3d import ecef
 
 sys.path.append('.')
 from noaatools import export_czml
+from noaatools.constants import *
 
 class Method(Enum):
     SPHERICAL = 1
     OBLATE = 2
     PYMAP3D = 3
-
-# Conversion between radians and degrees
-DEG2RAD = 0.017453292519943296
-RAD2DEG = 57.295779513082321
-
-RE = 6378.137 # Earth radius (in km)
-
-# This defines ellipsoid (a = equatorial radius in km, finv = inverse flattening)
-Ellipsoid = namedtuple('Ellipsoid', "a finv")
-
-# Source: https://en.wikipedia.org/wiki/Earth_ellipsoid#Historical_Earth_ellipsoids
-ellipsoid_wgs84 = Ellipsoid(a = RE, finv = 298.257223563)
-
-# Sat image aquisition and image transmission is not instanteneous. There is some delay. It's small, but
-# bacause the sat moves at speeds of more than 7km/s, even fractions of second make a big deal of a difference.
-# Not sure if this data is available anywhere. I think it'll have to be picked with trial-and-error.
-# This is expressed in seconds.
-NOAA_PROCESSING_DELAY = 0.0
-
-# This is the FOV (field of view) angle for the observation instrument on NOAA sats for each side.
-# In simple words this is how far the camera can look sideways. The total viewing angle is this multipled by 2.
-AVHRR_ANGLE = 54.3
 
 # Nice conversions: https://github.com/skyfielders/python-skyfield/blob/master/skyfield/sgp4lib.py
 # Good explanation: https://stackoverflow.com/questions/8233401/how-do-i-convert-eci-coordinates-to-longitude-latitude-and-altitude-to-display-o
