900b74928c28eaf104acefe4763eb128b06dd33f
Tomek Mrugalski <tomasz.mrugalski@eti.pg.edu.pl>
2020-05-17 13:52:52 +0200

    __init__.py added, obsolete path tricks removed

---
+++ noaatools/__init__.py
@@ -0,0 +1,4 @@
+"""
+**NOAA-TOOLS** is a small library to work with satellites images received from NOAA satellites.
+
+"""
--- noaatools/noaatools.py
+++ noaatools/noaatools.py
@@ -1,10 +1,6 @@
 import sys
 import os
 
-PACKAGE_PARENT = '..'
-SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
-sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
-
 APP_NAME = "noaatools"
 
 # This script processes images received from NOAA satellites
